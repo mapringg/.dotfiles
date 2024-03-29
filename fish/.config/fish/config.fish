9b8d4e02ec5b3b5d99153230c20fd0a356c413fe
Sorawit Kongnurat <work@sorawitk.com>
2024-03-29 12:04:01 +0700

    add fish shell for cachyos

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,12 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+alias lg='lazygit'
+alias ld='lazydocker'
+alias e='gh copilot explain'
+alias s='gh copilot suggest'
