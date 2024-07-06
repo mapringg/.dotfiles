2e97bf700c6c6f53c63a2b95b786a44ae8d4705a
Sorawit Kongnurat <work@sorawitk.com>
2024-07-06 18:14:38 +0700

    show border around cmp

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,8 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Add border for diagnostics float
 vim.diagnostic.config({
   float = {
     border = "rounded",
   },
 })
+
+-- Make the cmp menu transparent
+vim.opt.pumblend = 0
