b8821189f5cb7589e8a35cf5279981a450261fc2
Sorawit Kongnurat <work@sorawitk.com>
2024-07-08 01:13:55 +0700

    adjust mongosh install for arch

--- install/mongosh.sh
+++ install/mongosh.sh
@@ -1,6 +1,8 @@
 cd /tmp
 MONGOSH_VERSION=$(curl -s "https://api.github.com/repos/mongodb-js/mongosh/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-wget -O mongosh.deb "https://github.com/mongodb-js/mongosh/releases/latest/download/mongodb-mongosh_${LATEST_RELEASE}_amd64.deb"
-sudo apt install -y ./mongosh.deb
-rm mongosh.deb
+curl -sLo mongosh.tar.gz "https://github.com/mongodb-js/mongosh/releases/latest/download/mongosh-${MONGOSH_VERSION}-linux-x64.tgz"
+tar -xf mongosh.tar.gz
+MONGOSH_DIR="mongosh-${MONGOSH_VERSION}-linux-x64"
+sudo install ${MONGOSH_DIR}/bin/mongosh /usr/local/bin
+rm -rf mongosh.tar.gz ${MONGOSH_DIR}
 cd -
