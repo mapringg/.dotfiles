6935e71c79014c69021a505e48216a078c47c858
Sorawit Kongnurat <work@sorawitk.com>
2024-07-07 10:54:06 +0700

    increase line height in wezterm

--- .config/wezterm/default.lua
+++ .config/wezterm/default.lua
@@ -6,6 +6,7 @@ function module.apply_to_config(config)
 	config.color_scheme = "Catppuccin Mocha"
 	config.enable_tab_bar = false
 	config.font = wezterm.font("MonoLisa")
+	config.line_height = 1.1
 	config.window_close_confirmation = "NeverPrompt"
 end
 
