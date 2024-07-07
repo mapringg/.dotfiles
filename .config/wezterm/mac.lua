3774559f402553f9a12a1ef51b91866b0781392f
Sorawit Kongnurat <work@sorawitk.com>
2024-07-07 17:53:30 +0700

    format wezterm configs, reorganize keybinds, and set extra defaults

--- .config/wezterm/mac.lua
+++ .config/wezterm/mac.lua
@@ -4,55 +4,55 @@ local keys = require("keys")
 local module = {}
 
 function module.apply_to_config(config)
-	config.keys = {
-		keys.cmd_to_tmux_prefix("1", "1"),
-		keys.cmd_to_tmux_prefix("2", "2"),
-		keys.cmd_to_tmux_prefix("3", "3"),
-		keys.cmd_to_tmux_prefix("4", "4"),
-		keys.cmd_to_tmux_prefix("5", "5"),
-		keys.cmd_to_tmux_prefix("6", "6"),
-		keys.cmd_to_tmux_prefix("7", "7"),
-		keys.cmd_to_tmux_prefix("8", "8"),
-		keys.cmd_to_tmux_prefix("9", "9"),
-		keys.cmd_to_tmux_prefix("t", "c"),
-		keys.cmd_to_tmux_prefix("T", "!"),
-		keys.cmd_to_tmux_prefix("w", "x"),
-		keys.cmd_to_tmux_prefix("k", "k"),
-		keys.cmd_to_tmux_prefix("-", '"'),
-		keys.cmd_to_tmux_prefix("d", "d"),
-		keys.cmd_to_tmux_prefix("z", "z"),
+  config.keys = {
+    keys.cmd_to_tmux_prefix("1", "1"),
+    keys.cmd_to_tmux_prefix("2", "2"),
+    keys.cmd_to_tmux_prefix("3", "3"),
+    keys.cmd_to_tmux_prefix("4", "4"),
+    keys.cmd_to_tmux_prefix("5", "5"),
+    keys.cmd_to_tmux_prefix("6", "6"),
+    keys.cmd_to_tmux_prefix("7", "7"),
+    keys.cmd_to_tmux_prefix("8", "8"),
+    keys.cmd_to_tmux_prefix("9", "9"),
+    keys.cmd_to_tmux_prefix("t", "c"),
+    keys.cmd_to_tmux_prefix("T", "!"),
+    keys.cmd_to_tmux_prefix("w", "x"),
+    keys.cmd_to_tmux_prefix("k", "k"),
+    keys.cmd_to_tmux_prefix("-", '"'),
+    keys.cmd_to_tmux_prefix("d", "d"),
+    keys.cmd_to_tmux_prefix("z", "z"),
     keys.cmd_key(
-			"s",
-			wezterm_action.Multiple({
-				wezterm_action.SendKey({ key = "\x1b" }), -- escape
-				keys.multiple_actions(":w"),
-			})
-		),
-		{
-			mods = "CMD|SHIFT",
-			key = "|",
-			action = wezterm_action.Multiple({
-				wezterm_action.SendKey({ mods = "CTRL", key = "b" }),
-				wezterm_action.SendKey({ key = "%" }),
-			}),
-		},
-		{
-			mods = "CMD|SHIFT",
-			key = "}",
-			action = wezterm_action.Multiple({
-				wezterm_action.SendKey({ mods = "CTRL", key = "b" }),
-				wezterm_action.SendKey({ key = "n" }),
-			}),
-		},
-		{
-			mods = "CMD|SHIFT",
-			key = "{",
-			action = wezterm_action.Multiple({
-				wezterm_action.SendKey({ mods = "CTRL", key = "b" }),
-				wezterm_action.SendKey({ key = "p" }),
-			}),
-		},
-	}
+      "s",
+      wezterm_action.Multiple({
+        wezterm_action.SendKey({ key = "\x1b" }), -- escape
+        keys.multiple_actions(":w"),
+      })
+    ),
+    keys.key_table(
+      "CMD|SHIFT",
+      "|",
+      wezterm_action.Multiple({
+        wezterm_action.SendKey({ mods = "CTRL", key = "b" }),
+        wezterm_action.SendKey({ key = "%" }),
+      })
+    ),
+    keys.key_table(
+      "CMD|SHIFT",
+      "}",
+      wezterm_action.Multiple({
+        wezterm_action.SendKey({ mods = "CTRL", key = "b" }),
+        wezterm_action.SendKey({ key = "n" }),
+      })
+    ),
+    keys.key_table(
+      "CMD|SHIFT",
+      "{",
+      wezterm_action.Multiple({
+        wezterm_action.SendKey({ mods = "CTRL", key = "b" }),
+        wezterm_action.SendKey({ key = "p" }),
+      })
+    ),
+  }
 end
 
 return module
