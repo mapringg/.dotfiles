a937fe06662428db5332b2966f70b23ac38beb1c
Sorawit Kongnurat <work@sorawitk.com>
2024-07-09 01:53:36 +0700

    update wezterm default configs

--- .config/wezterm/default.lua
+++ .config/wezterm/default.lua
@@ -3,23 +3,10 @@ local wezterm = require("wezterm")
 local module = {}
 
 function module.apply_to_config(config)
-  config.color_scheme = "Catppuccin Mocha"
-  config.enable_tab_bar = false
   config.font = wezterm.font("MonoLisa")
   config.font_size = 11
-  config.line_height = 1.1
-  config.bold_brightens_ansi_colors = true
   config.window_close_confirmation = "NeverPrompt"
-  config.front_end = "WebGpu"
-  config.webgpu_power_preference = "HighPerformance"
-  config.cursor_blink_ease_in = "Constant"
-  config.cursor_blink_ease_out = "Constant"
-  config.underline_thickness = 3
-  config.cursor_thickness = 4
-  config.underline_position = -6
-  config.default_cursor_style = "BlinkingBar"
-  config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
-  config.scrollback_lines = 10000
+  config.hide_tab_bar_if_only_one_tab = true
 end
 
 return module
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local default = require("default")
-local mac = require("mac")
+-- local mac = require("mac")
 local config = {}
 default.apply_to_config(config)
-mac.apply_to_config(config)
+-- mac.apply_to_config(config)
 return config
