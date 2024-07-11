bf5767f456953652b7547502690dd53f796a40db
Sorawit Kongnurat <work@sorawitk.com>
2024-07-11 22:08:37 +0700

    add command for gnome to disable suspend on login page

--- install/gnome.sh
+++ install/gnome.sh
@@ -1,3 +1,4 @@
 dconf reset -f /
 gsettings set org.gnome.shell app-picker-layout "[]"
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+sudo -u gdm dbus-run-session gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
