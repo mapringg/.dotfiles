e1dda473593438766c93078762b7d8cebda8c6ea
Sorawit Kongnurat <work@sorawitk.com>
2024-07-14 18:28:19 +0700

    update ufw setup

--- install/ufw.sh
+++ install/ufw.sh
@@ -1,9 +1,6 @@
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 
-sudo ufw allow syncthing
-
-sudo ufw allow in on tailscale0 to any port 22
-sudo ufw allow 41641/udp
+sudo ufw allow in on tailscale0
 
 sudo ufw enable
