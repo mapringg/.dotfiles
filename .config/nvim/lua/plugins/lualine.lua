e8e2b2b95360ff32b53c34fc7e8ebd5fcc1b4f7b
Sorawit Kongnurat <work@sorawitk.com>
2024-07-20 17:50:54 +0000

    enable lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,31 +1,9 @@
-local icons = LazyVim.config.icons
-
 return {
   "nvim-lualine/lualine.nvim",
-  enabled = false,
   opts = {
     options = {
       component_separators = "",
-      section_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
     },
-    sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = {
-        {
-          "diagnostics",
-          symbols = {
-            error = icons.diagnostics.Error,
-            warn = icons.diagnostics.Warn,
-            info = icons.diagnostics.Info,
-            hint = icons.diagnostics.Hint,
-          },
-        },
-        { LazyVim.lualine.pretty_path() },
-      },
-      lualine_y = {},
-      lualine_z = {},
-    },
-    extensions = { "lazy" },
   },
 }
