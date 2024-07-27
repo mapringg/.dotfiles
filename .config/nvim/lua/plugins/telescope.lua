9e8217bc88851824f195a24d16c20b36e46a81ab
Sorawit Kongnurat <work@sorawitk.com>
2024-07-27 21:40:32 +0700

    show filename first in telescope

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      path_display = {
+        filename_first = {
+          reverse_directories = false,
+        },
+      },
+    },
+  },
+}
