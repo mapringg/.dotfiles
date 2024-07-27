275d263de880726e975c689862ecebe0023cad03
Sorawit Kongnurat <work@sorawitk.com>
2024-07-27 20:42:11 +0700

    update remaining install scripts to tar

--- install/gh.sh
+++ install/gh.sh
@@ -1,6 +1,7 @@
 cd /tmp
 GH_VERSION=$(curl -s "https://api.github.com/repos/cli/cli/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-wget -O gh.deb "https://github.com/cli/cli/releases/latest/download/gh_${GH_VERSION}_linux_amd64.deb"
-sudo apt install -y ./gh.deb
-rm gh.deb
+wget -O gh.tar.gz "https://github.com/cli/cli/releases/latest/download/gh_${GH_VERSION}_linux_amd64.tar.gz"
+tar -xf gh.tar.gz
+sudo install gh_${GH_VERSION}_linux_amd64/bin/gh /usr/local/bin
+rm -rf gh.tar.gz gh_${GH_VERSION}_linux_amd64
 cd -
--- install/gum.sh
+++ install/gum.sh
@@ -1,6 +1,7 @@
 cd /tmp
 GUM_VERSION=$(curl -s "https://api.github.com/repos/charmbracelet/gum/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-wget -O gum.deb "https://github.com/charmbracelet/gum/releases/latest/download/gum_${GUM_VERSION}_amd64.deb"
-sudo apt install -y ./gum.deb
-rm gum.deb
+wget -O gum.tar.gz "https://github.com/charmbracelet/gum/releases/latest/download/gum_${GUM_VERSION}_Linux_x86_64.tar.gz"
+tar -xf gum.tar.gz
+sudo install gum_${GUM_VERSION}_Linux_x86_64/gum /usr/local/bin
+rm -rf gum.tar.gz gum_${GUM_VERSION}_Linux_x86_64
 cd -
