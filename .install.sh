27cb50b24e3d470b400fc37564ba19f4bd1807ef
Sorawit Kongnurat <work@sorawitk.com>
2024-07-28 18:29:06 +0700

    rename update script to install script

--- .install.sh
+++ .install.sh
@@ -1,5 +1,7 @@
 cd ~/.dotfiles
 
+sudo apt update && sudo apt upgrade -y
+
 sudo apt install -y \
   stow curl xsel \
   fd-find bat zsh python3-venv \
@@ -9,6 +11,7 @@ sudo apt install -y \
 sudo ln -s /usr/bin/batcat /usr/bin/bat
 sudo ln -s /usr/bin/fdfind /usr/bin/fd
 
+mkdir -p ~/.config
 stow . -t ~
 for file in install/*.sh; do bash "$file"; done
 
@@ -16,8 +19,10 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 bat cache --build
 
 mise install
+chsh
 
 echo "Please install tmux plugins by pressing prefix + I"
 echo "Please install neovim plugins by opening neovim"
+echo "Please run gh auth login to authenticate with GitHub"
 
 cd -
