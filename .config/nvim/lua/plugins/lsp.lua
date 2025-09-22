3105c6245ca3a138f8e875a5819147d4a951c04d
Sorawit Kongnurat <sorawit.k@tutanota.com>
2025-09-22 16:48:09 +0700

    add graphql to neovim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,6 +2,7 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
+      graphql = {},
       stylelint_lsp = {
         filetypes = { "html", "css", "less", "scss" },
         settings = {},
