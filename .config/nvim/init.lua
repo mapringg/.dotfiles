277023c7a9d54c31c78e382fcbbfe81451df88ed
Sorawit Kongnurat <sorawit.k@tutanota.com>
2025-11-22 13:09:16 +0700

    update neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,10 +11,6 @@ vim.opt.clipboard = "unnamedplus"
 
 vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
 vim.cmd("hi NonText guibg=NONE ctermbg=NONE")
-vim.cmd("hi StatusLine guibg=NONE ctermbg=NONE")
 vim.cmd("hi StatusLineNC guibg=NONE ctermbg=NONE")
 vim.cmd("hi SignColumn guibg=NONE")
 
-vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Save file' })
-vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = 'Quit' })
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', { desc = 'Clear search highlights' })
