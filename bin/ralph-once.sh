58cd4125f76bd0f2a6c8d122b056e99d892a6437
Sorawit Kongnurat <sorawit.k@tutanota.com>
2026-01-07 11:47:28 +0700

    run ralph without permission

--- bin/ralph-once.sh
+++ bin/ralph-once.sh
@@ -1,6 +1,6 @@
 set -e
 
-claude --permission-mode acceptEdits "@plans/prd.json @progress.txt \
+claude --dangerously-skip-permissions "@plans/prd.json @progress.txt \
 1. Find the highest priority feature to work on and work only on that feature. \
 This should be the one YOU decide has the highest priority, not necessarily the first in the list. \
 2. Check that the types check via pnpm typecheck and that the tests pass via pnpm test. \
--- bin/ralph.sh
+++ bin/ralph.sh
@@ -8,7 +8,7 @@ fi
 for ((i = 1; i <= $1; i++)); do
 	echo "Iteration $i"
 	echo "---------------------------------"
-	result=$(claude --permission-mode acceptEdits -p "@plans/prd.json @progress.txt \
+	result=$(claude --dangerously-skip-permissions -p "@plans/prd.json @progress.txt \
 1. Find the highest priority feature to work on and work only on that feature. \
 This should be the one YOU decide has the highest priority, not necessarily the first in the list. \
 2. Check that the types check via pnpm typecheck and that the tests pass via pnpm test. \
