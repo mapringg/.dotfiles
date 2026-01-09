f06d6e7b35d4416221a66f3a997f0514509282f7
Sorawit Kongnurat <sorawit.k@tutanota.com>
2026-01-09 16:46:02 +0700

    customize hydro prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,6 +12,15 @@ set -gx SUDO_EDITOR $EDITOR
 set -gx BAT_THEME ansi
 fish_add_path -g $HOME/bin
 
+# Hydro customization
+set -g hydro_color_pwd '#fabd2f'
+set -g hydro_color_git '#b8bb26'
+set -g hydro_color_prompt '#928374'
+set -g hydro_color_error '#fb4934'
+set -g hydro_color_duration '#fe8019'
+set -g hydro_fetch true
+set -g fish_prompt_pwd_dir_length 0
+
 # Tool initializations
 if command -q mise
     mise activate fish | source
