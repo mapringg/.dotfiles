f3d868d4e7643d53f860fc48f1150ea0ef838b67
Sorawit Kongnurat <sorawit.k@tutanota.com>
2026-01-10 11:51:08 +0700

    swap to catppuccin latte

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,17 +12,6 @@ set -gx SUDO_EDITOR $EDITOR
 set -gx BAT_THEME ansi
 fish_add_path -g $HOME/bin
 
-# Hydro customization
-set -g hydro_symbol_prompt ''
-set -g hydro_color_pwd '#e5c890'
-set -g hydro_color_git '#a6d189'
-set -g hydro_color_prompt '#a5adce'
-set -g hydro_color_error '#e78284'
-set -g hydro_color_duration '#f4b8e4'
-set -g hydro_fetch true
-set -g hydro_multiline true
-set -g fish_prompt_pwd_dir_length 0
-
 # Tool initializations
 if command -q mise
     mise activate fish | source
