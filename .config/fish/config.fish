c8195a3dd25ddb352fabbc420faffe08079319fd
Sorawit Kongnurat <sorawit.k@tutanota.com>
2026-01-11 21:24:59 +0700

    configure fd, ripgrep, and fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,6 +11,12 @@ set -gx EDITOR vim
 set -gx SUDO_EDITOR $EDITOR
 set -gx BAT_THEME ansi
 set -gx SSH_AUTH_SOCK ~/.bitwarden-ssh-agent.sock
+set -gx RIPGREP_CONFIG_PATH ~/.config/ripgrep/ripgreprc
+
+# fzf uses fd for faster file/directory searching
+set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden'
+set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+set -gx FZF_ALT_C_COMMAND 'fd --type d --hidden'
 
 # Tool initializations
 if command -q mise
