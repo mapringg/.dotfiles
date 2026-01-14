5f57129559876292c2f998c11d41ec597f2ef4f8
Sorawit Kongnurat <sorawit.k@tutanota.com>
2026-01-14 22:48:02 +0700

    remove tmux config and tmproj script

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -7,9 +7,3 @@ abbr -a br brew
 # Navigation
 abbr -a cdev 'cd ~/Developer'
 abbr -a cdot 'cd ~/.dotfiles'
-
-# Tmux
-abbr -a t tmux
-abbr -a ta 'tmux attach'
-abbr -a tl 'tmux list-sessions'
-abbr -a tp tmproj
