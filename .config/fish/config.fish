2270dc638dd704263a8e73292d340d14981b61be
Sorawit Kongnurat <sorawit.k@tutanota.com>
2026-01-23 17:24:16 +0700

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,9 @@ if test -f /opt/homebrew/bin/brew
     eval (/opt/homebrew/bin/brew shellenv)
 end
 
+# Path
+fish_add_path -g ~/.local/bin
+
 # Environment
 if command -q nvim
     set -gx EDITOR nvim
@@ -18,6 +21,14 @@ end
 set -gx SUDO_EDITOR $EDITOR
 set -gx BAT_THEME ansi
 set -gx HOMEBREW_NO_ENV_HINTS 1
+set -gx SSH_AUTH_SOCK ~/.bitwarden-ssh-agent.sock
+set -gx RIPGREP_CONFIG_PATH ~/.config/ripgrep/ripgreprc
+
+# Hydro prompt
+set -g hydro_color_pwd cyan
+set -g hydro_color_git cyan
+set -g hydro_color_prompt cyan
+set -g hydro_color_error cyan
 
 # Aliases
 if command -q eza
@@ -28,6 +39,7 @@ if command -q eza
 end
 
 alias ff "fzf --preview 'bat --style=numbers --color=always {}'"
+alias fd 'fd --hidden --ignore-case'
 
 alias .. 'cd ..'
 alias ... 'cd ../..'
@@ -37,7 +49,7 @@ alias c opencode
 alias d docker
 alias l lazygit
 
-# Tool init
+# Tool init (keep last - some override aliases like cd)
 if command -q zoxide
     zoxide init fish --cmd cd | source
 end
@@ -45,9 +57,3 @@ end
 if command -q fzf
     fzf --fish | source
 end
-
-# Personal
-fish_add_path -g ~/.local/bin
-set -gx SSH_AUTH_SOCK ~/.bitwarden-ssh-agent.sock
-set -gx RIPGREP_CONFIG_PATH ~/.config/ripgrep/ripgreprc
-alias fd 'fd --hidden --ignore-case'
