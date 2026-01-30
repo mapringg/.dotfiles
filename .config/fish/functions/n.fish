a1844fa620f17afb9b6640f93f7c8a134d3f830f
Sorawit Kongnurat <sorawit.k@tutanota.com>
2026-01-30 15:52:37 +0700

    remove description from functions

--- .config/fish/functions/n.fish
+++ .config/fish/functions/n.fish
@@ -1,7 +1,8 @@
-function n --description 'Open neovim with args or current directory'
+function n
     if test (count $argv) -eq 0
         nvim .
     else
         nvim $argv
     end
 end
+
--- .config/fish/functions/tmux-sessionizer.fish
+++ .config/fish/functions/tmux-sessionizer.fish
@@ -1,4 +1,4 @@
-function tmux-sessionizer --description "Open tmux sessionizer"
+function tmux-sessionizer
     command tmux-sessionizer
     commandline -f repaint
 end
